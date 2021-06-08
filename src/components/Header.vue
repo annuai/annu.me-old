fd590ff0f15b8c566b792cde598dbf9c9688bafe
Annuai <annuai.annu@gmail.com>
2021-06-08 23:05:38 +0530

    Added Resume

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,8 @@
             </div>
             <nav class="nav right">
                 <g-link class="nav__link" to="/journal">Journal</g-link>
-                <g-link class="nav__link" to="/contact">Say Hi!</g-link>
+                <g-link class="nav__link" to="/resume">Resume</g-link>
+                <g-link class="nav__link" to="/contact">Contact</g-link>
             </nav>
         </div>
     </header>
